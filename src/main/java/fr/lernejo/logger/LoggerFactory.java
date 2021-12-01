bb3d5099b6c1679d5a88609585e504d184f25465
devstra <oscar.f@outlook.fr>
2021-12-01 18:56:11 +0100

    Added FilteredLogger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,7 +1,12 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        return new CompositeLogger(new ContextualLogger(name, new FileLogger("logs.txt")), new ContextualLogger(name, new ConsoleLogger()));
+        Predicate<String> fileLoggerFilter = msg -> msg.contains("simulation");
+        return new CompositeLogger(new ContextualLogger(name, new FilteredLogger(new FileLogger("logs.txt"), fileLoggerFilter)), new ContextualLogger(name, new ConsoleLogger()));
+
+        //        return new CompositeLogger(new FilteredLogger(new ContextualLogger(name, new FileLogger("logs.txt")), fileLoggerFilter), new ContextualLogger(name, new ConsoleLogger()));
     }
 }
